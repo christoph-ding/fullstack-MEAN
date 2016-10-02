319e1871a937e730228cf361b4139dcb335f01aa
Pokegear <christoph.ding@gmail.com>
2016-10-02 14:18:56 -0400

    Add get end-point for the root dictectory.

--- server/server.js
+++ server/server.js
@@ -5,3 +5,10 @@ var app = express();
 app.listen(8000, function() {
   console.log('Listening');
 });
+
+// really simple routing
+app.get('/', function(req, res) {
+  console.log('got to the root GET endpoint');
+  res.status(200);
+  res.send('this is the response for the root GET endpoint');
+});
