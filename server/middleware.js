99bd58bbae91a37d573c560edea2df741328a695
Pokegear <christoph.ding@gmail.com>
2016-10-02 19:32:38 -0400

    Add a end request response cycle function that takes the response sending responsbility.

--- server/middleware.js
+++ server/middleware.js
@@ -6,12 +6,17 @@ var sayHello = function(req, res, next) {
   next();
 }
 
+var endResReqCycle = function(req, res) {
+  var msg = res.locals.msg;
+  res.send(msg);
+}
+
 module.exports = function(app, express) {
   app.use(morgan('dev'));
   app.use(bodyParser.json());
 
   // routing
   var cookieRouter = require('./services/cookies/cookiesRouter.js')(express);
-  app.use('/cookies', sayHello, cookieRouter);
+  app.use('/cookies', sayHello, cookieRouter, endResReqCycle);
 
 }
--- server/services/cookies/cookiesController.js
+++ server/services/cookies/cookiesController.js
@@ -1,7 +1,13 @@
 var returnFlavor = function(req, res, next) {
   var flavor = req.body.flavor;
-  res.status(200);
-  res.send('have a ' + flavor + ' cookie!\n');
+
+  if (flavor === 'undefined') {
+    res.status(400);
+    res.send('you did not choose a flavor\n');
+  } else {
+    res.locals.msg = 'you chose a ' + flavor + ' cookie!\n';
+    next();
+  }
 }
 
 module.exports = {
