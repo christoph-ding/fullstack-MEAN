d45a486049f10a5fdec08581bf6803d23daebeeb
Pokegear <christoph.ding@gmail.com>
2016-10-02 14:42:59 -0400

    Add morgan to server.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var morgan = require('morgan');
 
 var app = express();
 
 app.listen(8000, function() {
   console.log('Listening');
+});
+
+app.use(morgan('dev'));
+
+app.use(function(req, res, next) {
+  console.log('reached the app server!');
+  next();
 });
 
 // really simple routing
@@ -12,3 +20,4 @@ app.get('/', function(req, res) {
   res.status(200);
   res.send('this is the response for the root GET endpoint');
 });
+
